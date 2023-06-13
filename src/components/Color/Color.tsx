9aba15da49e0588d177886543f21d67914ef2a4b
Realis813 <82963462+Realis813@users.noreply.github.com>
2023-06-14 00:28:04 +0200

    zmiany w Color

--- src/components/Color/Color.tsx
+++ src/components/Color/Color.tsx
@@ -1,22 +1,43 @@
-import { CSSProperties } from "react";
-import "./Color.scss"
+import { CSSProperties, Dispatch, SetStateAction } from "react";
+import "./Color.scss";
 
-export const Color = ({color}:{color:string}) => {
-   
-    return (
-        
-        <div>
-            <div className="color-rectangle" style={{"--bg-color":color} as CSSProperties }>
-             
-
-            </div>
-            <p>
-                {
-                    color.toUpperCase()
-                }
-            </p>
-        </div>
-     );
-  };
-  
- 
+export const Color = ({
+  color,
+  colors,
+  colorsSetter,
+}: {
+  color: string;
+  colors: string[];
+  colorsSetter: Dispatch<SetStateAction<string[]>>;
+}) => {
+  const arrayOfColorsFromLocalStorage: string[] = JSON.parse(
+    localStorage.getItem("arrayOfColors") || "[]"
+  );
+  return (
+    <div className="color">
+      <div
+        className="color-rectangle"
+        style={{ "--bg-color": color } as CSSProperties}
+      >
+        {arrayOfColorsFromLocalStorage.includes(color) && (
+          <button
+            onClick={() => {
+              localStorage.setItem(
+                "arrayOfColors",
+                JSON.stringify(
+                  arrayOfColorsFromLocalStorage.filter(
+                    (value) => value !== color
+                  )
+                )
+              );
+              colorsSetter(colors.filter((value) => value !== color));
+            }}
+          >
+            X
+          </button>
+        )}
+      </div>
+      <p>{color.toUpperCase()}</p>
+    </div>
+  );
+};
